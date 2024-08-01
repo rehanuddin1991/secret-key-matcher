0498fdd8ab8b01955972143c2578b5bfc61e8d4f
rehanuddin1991 <rehan.doict@gmail.com>
2024-08-02 00:15:14 +0600

    clear and X function added

--- script.js
+++ script.js
@@ -19,14 +19,16 @@ document.getElementById("secret_key_button").addEventListener("click", (e) => {
 
 /* input keys and display value and check with secret key function */
 const container_div = document.getElementById("input_output_div");
-const result = document.getElementById("display_value");
-let value = "";
+const result = document.getElementById("display_value");  
+//let value = "";
 
 container_div.addEventListener("click", (e) => {
-
+let length_check="";
 
     if (e.target.matches("button")) {
-        console.log(e.target.innerText);
+        length_check=result.value +'';
+        if(length_check.length>5 && e.target.innerText != "Clear" && e.target.innerText != "Submit" && e.target.innerText != "X") alert("Only 6 digits allowed");
+        //console.log(e.target.innerText);
         //e.target.style.backgroundColor="red";
         //console.log(document.getElementById("result").innerText);
         if (e.target.innerText == "Submit") {
@@ -37,12 +39,42 @@ container_div.addEventListener("click", (e) => {
 
         }
 
-        else if (e.target.innerText == "Clear") result.innerText = '';
+        else if (e.target.innerText == "Clear") 
+            {
+                result.value = '';
+            }
+            else if (e.target.innerText=="X")
+            {
+                deleteLastString(result.value);
+            }
         else {
-            if (value.length < 6)
-                value += e.target.innerText;
+            result.value+='';
+            //console.log(result.value);
+            if (result.value.length < 6)
+            {
+                result.value += e.target.innerText;
+                 
+            }
+               
         }
 
     }
-    result.value = value;
+    
 });
+
+
+function deleteLastString(str)
+{  
+    if(str)
+        {
+            let len = str.length;
+            let arr = [];
+            for (let i = 0; i < len-1; i++) {
+                arr.push(str[i]);
+            }
+            document.getElementById("display_value").value=arr.join("") ;
+        }   
+
+
+
+}
